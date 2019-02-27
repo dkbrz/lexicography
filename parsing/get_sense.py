233c60e44eeb36edb809164f610be4597a7ba62a
dkbrz <glaz.dikobraz@gmail.com>
2019-02-27 09:53:32 +0300

    sorting fix

--- parsing/get_sense.py
+++ parsing/get_sense.py
@@ -59,7 +59,7 @@ def main():
     folder_sense = sys.argv[2]
     lang = sys.argv[3]
     files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and 'txt' in f]
-    files.sort(key=lambda x: int(x.split('.')[0].split('_')[1]))
+    files.sort(key=lambda x: int(re.findall('[0-9]+', x)[0]))
     print('Joining files...')
     for file in files:
         with open('{}/{}'.format(folder, file), 'r') as f_r, open('{}/train.txt'.format(folder), 'a') as f_w:

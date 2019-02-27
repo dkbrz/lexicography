f1841fb1c98f7532e109d6ffee747b6a9b374498
panteleeva48 <irapanteleyeva@rambler.ru>
2019-02-27 23:09:13 +0300

    Add files via upload

---
+++ parsing/sense_every_file.py
@@ -0,0 +1,68 @@
+import sys
+import os
+import numpy as np
+import adagram
+
+WINDOW_SIZE = 5
+
+
+def find_context(ind, words, window=WINDOW_SIZE):
+    if ind - window < 0:
+        start = 0
+    else:
+        start = ind - window
+
+    if ind + window > len(words):
+        end = len(words) + 1
+    else:
+        end = ind + window + 1
+
+    left = words[start:ind]
+    right = words[ind + 1:end]
+    return left + right
+
+
+def find_sense(word, context, model):
+    try:
+        probs = model.disambiguate(word, context)
+        sense = np.argmax(probs, axis=0)
+    except:
+        sense = 0
+    return str(sense)
+
+
+def sense_line(line, model, window=WINDOW_SIZE):
+    new_line = ''
+    words = line.split()
+    for ind, word in enumerate(words):
+        context = find_context(ind, words, window=window)
+        sense = find_sense(word, context, model=model)
+        word = word + '_' + sense
+        new_line += word + ' '
+    return new_line.strip()
+
+
+def sense_file(file, model, folder, folder_sense, window=WINDOW_SIZE):
+    with open('{}/{}'.format(folder, file), 'r') as file_r, open('{}/sense_'.format(folder_sense) + file, 'a') as file_w:
+        for line in file_r:
+            new_file = sense_line(line, model=model, window=window)
+            file_w.write(new_file + '\n')
+    return file
+
+
+def main():
+    folder = sys.argv[1]  # where small files with lemma_pos
+    folder_sense = sys.argv[2]  # new folder for disambiguated files
+    vm = sys.argv[3]  # model
+
+    vm = adagram.VectorModel.load(vm)
+    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and 'lemma_upostag' in f]
+    files.sort(key=lambda x: int(x.split('.')[0].split('_')[1]))
+    if not os.path.exists(folder_sense):
+        os.mkdir(folder_sense)
+    for f in files:
+        sense_file(f, vm, folder, folder_sense, window=WINDOW_SIZE)
+
+
+if __name__ == "__main__":
+    main()

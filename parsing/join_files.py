f315878f650fdfd36610c29f37fcb0a0e10325b5
dkbrz <glaz.dikobraz@gmail.com>
2019-03-03 12:05:58 +0300

    join for align

--- parsing/join_files.py
+++ parsing/join_files.py
@@ -12,21 +12,21 @@ def main():
     args = parser.parse_args()
     lang_1 = args.lang_1
     lang_2 = args.lang_2
-    folder = '../languages/' + lang_1 + '_' + lang_2 + '/' + lang_1
-    if not os.path.isdir(folder):
-        folder = '../languages/' + lang_2 + '_' + lang_1 + '/' + lang_1
-    files = [f for f in os.listdir(folder + '/' + lang_1 + '_lemma_pos') if '.txt' in f]
+    folder = '../languages/' + lang_1 + '_' + lang_2 + '/'
+    files = [f for f in os.listdir(folder + '/' + lang_1 + '_sense') if '.txt' in f]
     files.sort(key=lambda x: int(re.findall('[0-9]+', x)[0]))
-    train_file = '{}/train.txt'.format(folder)
+    train_file = '{}/{}_align.txt'.format(folder, lang_1)
 
     print('Joining files...')
-    for file in files:
-        f = '{}/{}'.format(folder + '/' + lang_1 + '_lemma_pos', file)
+    for key, file in enumerate(files):
+        f = '{}/{}'.format(folder + '/' + lang_1 + '_sense', file)
         print('Adding file ' + f + '...')
         with open(f, 'r') as f_r,\
                 open(train_file, 'a') as f_w:
             info = f_r.read().rstrip()
-            f_w.write(info+'\n')
+            f_w.write(info)
+            if key != len(files)-1:
+                f_w.write('\n')
     print('File ' + train_file + ' is ready.')
 
 
